cc93d0ea147b7e9ddda529803c2fe04089cc0ffe
Devansh Agarwal <devansh.agarwal.official@gmail.com>
2024-10-20 13:52:49 +0530

    First commit

---
+++ lve_pipeline.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "lve_device.hpp"
+
+#include <string>
+#include <vector>
+
+namespace lve {
+    struct PipelineConfigInfo {
+      VkViewport viewport;
+      VkRect2D scissor;
+      VkPipelineViewportStateCreateInfo viewportInfo;
+      VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+      VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+      VkPipelineMultisampleStateCreateInfo multisampleInfo;
+      VkPipelineColorBlendAttachmentState colorBlendAttachment;
+      VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+      VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+      VkPipelineLayout pipelineLayout = nullptr;
+      VkRenderPass renderPass = nullptr;
+      uint32_t subpass = 0;
+    };
+
+    class LvePipeline {
+        public:
+            LvePipeline(LveDevice& device, std::string vertFilePath, const std::string fragFilePath, const PipelineConfigInfo configInfo);
+            ~LvePipeline();
+            
+            LvePipeline(const LvePipeline&) = delete;
+            void operator=(const LvePipeline&) = delete;
+
+            static PipelineConfigInfo defaultPipelineConfigInfo(u_int32_t width, u_int32_t height);
+
+        private:
+            static std::vector<char> readFile(const std::string& filePath);
+            void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo configInfo);
+
+            void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+            LveDevice& lveDevice;
+            VkPipeline graphicsPipeline;
+            VkShaderModule vertShaderModule;
+            VkShaderModule fragShaderModule;
+    };
+}
